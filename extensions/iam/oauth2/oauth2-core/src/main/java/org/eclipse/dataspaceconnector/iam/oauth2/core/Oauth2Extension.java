9f7c511851bfce2fa3e3c4baf14243b55f71a65b
Tim Berthold <tim.berthold@isst.fraunhofer.de>
2022-02-11 14:43:41 +0100

    refactor: ids token validation as extension

--- extensions/iam/oauth2/oauth2-core/src/main/java/org/eclipse/dataspaceconnector/iam/oauth2/core/Oauth2Extension.java
+++ extensions/iam/oauth2/oauth2-core/src/main/java/org/eclipse/dataspaceconnector/iam/oauth2/core/Oauth2Extension.java
@@ -25,6 +25,7 @@ import org.eclipse.dataspaceconnector.iam.oauth2.core.identity.Oauth2ServiceImpl
 import org.eclipse.dataspaceconnector.iam.oauth2.core.jwt.DefaultJwtDecorator;
 import org.eclipse.dataspaceconnector.iam.oauth2.core.jwt.JwtDecoratorRegistryImpl;
 import org.eclipse.dataspaceconnector.iam.oauth2.spi.JwtDecoratorRegistry;
+import org.eclipse.dataspaceconnector.iam.oauth2.spi.Oauth2Service;
 import org.eclipse.dataspaceconnector.spi.EdcException;
 import org.eclipse.dataspaceconnector.spi.EdcSetting;
 import org.eclipse.dataspaceconnector.spi.iam.IdentityService;
@@ -108,6 +109,7 @@ public class Oauth2Extension implements ServiceExtension {
         var oauth2Service = new Oauth2ServiceImpl(configuration, tokenSigner, okHttpClient, jwtDecoratorRegistry, context.getTypeManager());
 
         context.registerService(IdentityService.class, oauth2Service);
+        context.registerService(Oauth2Service.class, oauth2Service);
     }
 
     @Override
