7029fd1908a4c4d3a216e17226b651a465c21975
Jan Peter Meyer <jan.peter.meyer@isst.fraunhofer.de>
2022-09-09 17:17:15 +0200

    feat: mount files into container

--- samples/audit-logging/thirdPartyPrototype/app.py
+++ samples/audit-logging/thirdPartyPrototype/app.py
@@ -2,8 +2,11 @@ from flask import Flask
 from markupsafe import escape
 from flask import request
 from flask import Response
+import requests
 
 app = Flask(__name__)
+
+print("START SERVER")
 
 @app.route("/<filename>")
 def hello_world(filename):
@@ -17,18 +20,30 @@ def hello_world(filename):
 def get_file(filename):
     path = "files/" + escape(filename) + ".txt"
     with open(path) as f:
-        file = f.read()
-        data = {'file': file}
-        return Response(data, mimetype='multipart/form-data')
-
+        value = f.readlines()
+        return value
 
 
 @app.route('/file/<filename>', methods=['POST'])
 def post_file(filename):
-    data = request.files['file']
+    data = request.data
 
-    data.save('testSave.txt')
+    path = "files/" + escape(filename) + ".txt"
 
     print(data)
 
     return "Got Input"
+
+@app.get('/logging/create')
+def create_log():
+    # some JSON:
+    x = '{"sourceID" : "Meinasdb", "dataID" : "file.txt", "timestamp": "Jetszt", "logText" : "adsasd"}'
+
+    print(request.remote_addr)
+
+    headers = {'Content-Type': 'application/json'}
+    r = requests.post('http://'+ request.remote_addr + ':8183/logging/auditlogging/newLogs', data=x,headers=headers)
+
+    print(r)
+
+    return "Worked"
