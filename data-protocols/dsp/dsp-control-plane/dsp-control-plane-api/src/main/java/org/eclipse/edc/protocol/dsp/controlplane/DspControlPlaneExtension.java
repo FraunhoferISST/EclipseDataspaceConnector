2e8719749eeb14603de6a40b4ac4ded31a4bb305
Jan Peter Meyer <jan.peter.meyer@isst.fraunhofer.de>
2023-03-07 15:18:56 +0100

    feat: add controller Endpoints structure

--- data-protocols/dsp/dsp-control-plane/dsp-control-plane-api/src/main/java/org/eclipse/edc/protocol/dsp/controlplane/DspControlPlaneExtension.java
+++ data-protocols/dsp/dsp-control-plane/dsp-control-plane-api/src/main/java/org/eclipse/edc/protocol/dsp/controlplane/DspControlPlaneExtension.java
@@ -14,5 +14,55 @@
 
 package org.eclipse.edc.protocol.dsp.controlplane;
 
-public class DspControlPlaneExtension {
+import org.eclipse.edc.connector.contract.spi.negotiation.store.ContractNegotiationStore;
+import org.eclipse.edc.connector.contract.spi.validation.ContractValidationService;
+import org.eclipse.edc.connector.service.dataaddress.DataAddressValidatorImpl;
+import org.eclipse.edc.connector.service.transferprocess.TransferProcessServiceImpl;
+import org.eclipse.edc.connector.transfer.spi.TransferProcessManager;
+import org.eclipse.edc.connector.transfer.spi.store.TransferProcessStore;
+import org.eclipse.edc.protocol.dsp.api.configuration.DspApiConfiguration;
+import org.eclipse.edc.protocol.dsp.controlplane.controller.TransferProcessController;
+import org.eclipse.edc.runtime.metamodel.annotation.Inject;
+import org.eclipse.edc.spi.dataaddress.DataAddressValidator;
+import org.eclipse.edc.spi.system.ServiceExtension;
+import org.eclipse.edc.spi.system.ServiceExtensionContext;
+import org.eclipse.edc.spi.types.TypeManager;
+import org.eclipse.edc.transaction.spi.TransactionContext;
+import org.eclipse.edc.web.spi.WebService;
+
+public class DspControlPlaneExtension implements ServiceExtension {
+
+    @Inject
+    private TransferProcessStore transferProcessStore;
+
+    @Inject
+    private TransferProcessManager transferProcessManager;
+
+    @Inject
+    private TransactionContext transactionContext;
+
+    @Inject
+    private ContractNegotiationStore contractNegotiationStore;
+
+    @Inject
+    private ContractValidationService contractValidationService;
+
+    private final DataAddressValidator dataAddressValidator = new DataAddressValidatorImpl();
+
+    @Inject
+    private TypeManager typeManager;
+
+    @Inject
+    private DspApiConfiguration apiConfiguration;
+
+    @Inject
+    private WebService webService;
+
+    @Override
+    public void initialize(ServiceExtensionContext context) {
+        var transferProcessService = new TransferProcessServiceImpl(transferProcessStore, transferProcessManager, transactionContext,
+                contractNegotiationStore, contractValidationService, dataAddressValidator);
+        var transferProcessController = new TransferProcessController(context.getMonitor(), transferProcessService, typeManager);
+        webService.registerResource(apiConfiguration.getContextAlias(), transferProcessController);
+    }
 }
--- data-protocols/dsp/dsp-control-plane/dsp-control-plane-api/src/main/java/org/eclipse/edc/protocol/dsp/controlplane/controller/TransferProcessController.java
+++ data-protocols/dsp/dsp-control-plane/dsp-control-plane-api/src/main/java/org/eclipse/edc/protocol/dsp/controlplane/controller/TransferProcessController.java
@@ -14,13 +14,68 @@
 
 package org.eclipse.edc.protocol.dsp.controlplane.controller;
 
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.json.JsonObject;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+
+
+import org.eclipse.edc.connector.spi.transferprocess.TransferProcessService;
+import org.eclipse.edc.spi.monitor.Monitor;
+import org.eclipse.edc.spi.types.TypeManager;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Consumes({MediaType.APPLICATION_JSON})
 @Produces({MediaType.APPLICATION_JSON})
 @Path("/transfer-processes")
 public class TransferProcessController {
+
+    private Monitor monitor;
+
+    private TransferProcessService transferProcessService;
+
+    private ObjectMapper mapper;
+
+    public TransferProcessController(Monitor monitor, TransferProcessService transferProcessService, TypeManager typeManager) {
+        this.monitor = monitor;
+        this.transferProcessService = transferProcessService;
+        this.mapper = typeManager.getMapper("json-ld"); //TODO Use correct mapper
+    }
+
+    @GET
+    @Path("/{id}")
+    public Map<String, Object> getTransferProcess(@PathParam("id") String id) {
+        var transferProcess = transferProcessService.findById(id);
+        return mapper.convertValue(transferProcess,Map.class); //TODO Check if is works correct
+    }
+
+    @POST
+    @Path("/request")
+    public Map<String, Object> initiateTransferProcess(JsonObject jsonObject) {
+        return new HashMap<>();
+    }
+
+    @POST
+    @Path("/{id}/start")
+    public void consumerTransferProcessStart(JsonObject jsonObject) {
+    }
+
+    @POST
+    @Path("/{id}/completion")
+    public void consumerTransferProcessCompletion(JsonObject jsonObject) {
+    }
+
+    @POST
+    @Path("/{id}/termination")
+    public void consumerTransferProcessTermination(JsonObject jsonObject) {
+    }
+
+    @POST
+    @Path("/{id}/suspension")
+    public void consumerTransferProcessSuspension(JsonObject jsonObject) {
+
+    }
+
 }
